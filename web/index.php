2dd7a78c149ea2b5d0b93831dbe45629419a472a
Shane Michael Johnson <shane@MacBook-Air.local>
2021-09-09 07:47:38 -0500

    demo update

--- web/index.php
+++ web/index.php
@@ -2,29 +2,29 @@
 
 require('../vendor/autoload.php');
 
-header('location: MalexHTML/App/dist/index.html');
+//header('location: MalexHTML/App/dist/index.html');
 
 
-// $app = new Silex\Application();
-// $app['debug'] = true;
+$app = new Silex\Application();
+$app['debug'] = true;
 
-// // Register the monolog logging service
-// $app->register(new Silex\Provider\MonologServiceProvider(), array(
-//   'monolog.logfile' => 'php://stderr',
-// ));
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
 
-// // Register view rendering
-// $app->register(new Silex\Provider\TwigServiceProvider(), array(
-//     'twig.path' => __DIR__.'/MalexHTML/App/dist/index.html',
-// ));
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/MalexHTML/App/dist/index.twig',
+));
 
-// // Our web handlers
+// Our web handlers
 
-// $app->get('/', function() use($app) {
-//   $app['monolog']->addDebug('logging output.');
-//   return $app['twig']->render('index.twig');
-// });
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('index.twig');
+});
 
-// $app->run();
+$app->run();
 
 ?>
